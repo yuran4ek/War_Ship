738591f6ee333569f28528ff078318bb0e1fc52f
Yuri.Batanov <yurok_37@icloud.com>
2022-08-16 17:41:39 +0300

    Delete file static_method

--- static_method.py
+++
@@ -1,8 +0,0 @@
-try:
-    number = int(input("Input number: "))
-except ValueError as e:
-    print("Mistake in number")
-else:
-    print(f"You are enter the {number}")
-finally:
-    print("Close the programm")
